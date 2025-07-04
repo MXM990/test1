7fb3a0e93466789a75df5da100177d3f2ae6d63d
morerise <morerise@gmail.com>
2025-07-03 18:51:57 -0700

    add mxm class

---
+++ KN_3_test1/mxm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KN_3_test1
+{
+    internal class mxm
+    {
+        public void a()
+        {
+
+        }
+    }
+}
